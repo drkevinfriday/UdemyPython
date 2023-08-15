c0bd1f336fb52e3e7947c507eeb86dfc912966b6
drkevinfriday <kevinfriday87@gmail.com>
2023-08-15 14:38:39 -0500

    test

---
+++ Section5/number_list.py
@@ -0,0 +1,16 @@
+even =[2,3,4,6,8]
+odd = [1,3,5,7,9]
+
+print(min(even))
+print(max(even))
+print(min(odd))
+print(max(odd))
+
+print()
+print(len(odd))
+print(len(odd))
+
+print()
+
+print("mississippi".count("s"))
+print("mississippi".count("iss"))
