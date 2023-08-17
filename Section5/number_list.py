cec277ef72ae43016990f2efd84ffeccf8db5b7d
drkevinfriday <kevinfriday87@gmail.com>
2023-08-17 15:49:30 -0500

    lesson 122!

--- Section5/number_list.py
+++ Section5/number_list.py
@@ -1,16 +1,39 @@
 even =[2,3,4,6,8]
 odd = [1,3,5,7,9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
 
-print()
-print(len(odd))
-print(len(odd))
 
-print()
+numbers = [even , odd]
+print(numbers)
 
-print("mississippi".count("s"))
-print("mississippi".count("iss"))
+for  number_list in numbers: 
+    print(number_list)
+
+
+    for value in number_list: 
+        print(value)
+
+        
+# print(min(even))
+# print(max(even))
+# print(min(odd))
+# print(max(odd))
+
+# print()
+# print(len(odd))
+# print(len(odd))
+
+# print()
+
+# print("mississippi".count("s"))
+# print("mississippi".count("iss"))
+
+# even.extend(odd)
+# print(even)
+
+# even.sort()
+# print(even)
+# another_even = even 
+# print(another_even)
+# even.sort(reverse=True)
+# print(even)
